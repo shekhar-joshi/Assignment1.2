f23d22c1c3e79d748f36107d06b16210c432bb7d
shekhar-joshi <shekhar.joshi111@gmail.com>
2018-05-11 01:34:56 +0530

    Assignment 1.4
    Write a Python program to find the volume of a sphere with diameter 12 cm.
    Formula: V=4/3 * π * r**3

---
+++ Volume of sphere.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 11 01:26:43 2018
+
+@author: shekhar
+"""
+
+#volume of sphere
+print("volume of sphere")
+d=12
+r=d/2
+V=(4/3)*(3.142)*(r**3)
+print("Volume of sphere is :",V,"cm^3")
