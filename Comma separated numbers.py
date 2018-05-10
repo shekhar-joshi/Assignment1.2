e28b031696f8f3ff67085481169bd7d7631869dd
shekhar-joshi <shekhar.joshi111@gmail.com>
2018-05-11 01:49:44 +0530

    Assignment 1.5
    Write a program which accepts a sequence of comma-separated numbers from console
    and generate a list.

---
+++ Comma separated numbers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 11 01:39:06 2018
+
+@author: shekhar
+"""
+
+#comma separated numbers
+n=input("how many numbers?")
+x=[]
+for a in n:
+    a=input()
+    x.append(a)
+print(x)
+    
