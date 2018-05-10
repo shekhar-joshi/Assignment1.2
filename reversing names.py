9704ed8a210fc53c7f86ff0a8508b53b28df7b7e
shekhar-joshi <shekhar.joshi111@gmail.com>
2018-05-11 01:23:33 +0530

    Assignment 1.3
    Write a Python program to accept the user's first and last name and then getting them
    printed in the the reverse order with a space between first name and last name.

---
+++ reversing names.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 11 01:12:22 2018
+
+@author: shekhar
+"""
+a=input("first name?")
+b=input("last name?")
+
+print(''.join(reversed(a+' '+b)))
