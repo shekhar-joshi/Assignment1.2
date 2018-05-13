f9808c2eac9994531e8c1589f437aa3d8828f56a
shekhar-joshi <shekhar.joshi111@gmail.com>
2018-05-13 10:34:10 +0530

    Assignment 1.8
    print a line in a given sample format
    WE, THE PEOPLE OF INDIA, having solemnly resolved to constitute India into a SOVEREIGN! 
              SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC and 
                    to secure to all
    its citizens

---
+++ Printing a line in format.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 13 10:06:19 2018
+
+@author: shekhar
+"""
+
+print("WE, THE PEOPLE OF INDIA"+ 
+          " \n\thaving solemnly resolved to constitute India into a SOVEREIGN!"+
+          " \n\t\tSOCIALIST, SECULAR, DEMOCRATIC REPUBLIC and to secure to all its citizens")
